baa7f4c4b9aa27e77b8af68c8a2c2782797a66b9
Erick <Fernando>
2024-04-22 11:27:46 -0300

    pyinstallerCompatible

--- config.py
+++ config.py
@@ -1,20 +1,29 @@
+import os
+import sys
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
 
 CAPTION = "2048"
-ICON_IMAGE_SRC = "./sprites/block2048.png"
-IMAGE_2_SRC = "./sprites/block2.png"
-IMAGE_4_SRC = "./sprites/block4.png"
-IMAGE_8_SRC = "./sprites/block8.png"
-IMAGE_16_SRC = "./sprites/block16.png"
-IMAGE_32_SRC = "./sprites/block32.png"
-IMAGE_64_SRC = "./sprites/block64.png"
-IMAGE_128_SRC = "./sprites/block128.png"
-IMAGE_256_SRC = "./sprites/block256.png"
-IMAGE_512_SRC = "./sprites/block512.png"
-IMAGE_1024_SRC = "./sprites/block1024.png"
-IMAGE_2048_SRC = "./sprites/block2048.png"
-IMAGE_4096_SRC = "./sprites/block4096.png"
-IMAGE_8192_SRC = "./sprites/block8192.png"
-IMAGE_16384_SRC = "./sprites/block16384.png"
+ICON_IMAGE_SRC = resource_path("./sprites/block2048.png")
+IMAGE_2_SRC = resource_path("./sprites/block2.png")
+IMAGE_4_SRC = resource_path("./sprites/block4.png")
+IMAGE_8_SRC = resource_path("./sprites/block8.png")
+IMAGE_16_SRC = resource_path("./sprites/block16.png")
+IMAGE_32_SRC = resource_path("./sprites/block32.png")
+IMAGE_64_SRC = resource_path("./sprites/block64.png")
+IMAGE_128_SRC = resource_path("./sprites/block128.png")
+IMAGE_256_SRC = resource_path("./sprites/block256.png")
+IMAGE_512_SRC = resource_path("./sprites/block512.png")
+IMAGE_1024_SRC = resource_path("./sprites/block1024.png")
+IMAGE_2048_SRC = resource_path("./sprites/block2048.png")
+IMAGE_4096_SRC = resource_path("./sprites/block4096.png")
+IMAGE_8192_SRC = resource_path("./sprites/block8192.png")
+IMAGE_16384_SRC = resource_path("./sprites/block16384.png")
 FPS = 30
 WINDOW_SIZE = (356, 356)
 BACKGROUND_COLOR = "#dddddd"
